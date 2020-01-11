30b7170dd84fd92bdd478dffdc9cba8b8524e6fc
unknown <bhushank0029@gmail.com>
2020-01-11 16:32:47 +0530

    added new fucntion

---
+++ src/seleniumprogram/Radiobuttion.java
@@ -0,0 +1,10 @@
+package seleniumprogram;
+
+public class Radiobuttion {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("verify the radio buttion condition");
+	}
+
+}
--- src/seleniumprogram/Sample.java
+++ src/seleniumprogram/Sample.java
@@ -4,6 +4,8 @@ public class Sample {
 
 	public static void main(String[] args) {
 System.out.println("hellow jaga");
+System.out.println("sdkkdshfkdsf");
+System.out.println("sdfsldjfljsdf");
 	}
 
 }
