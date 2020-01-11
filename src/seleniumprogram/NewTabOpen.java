812692434a4031525a2c7f9e40ca0d7deab58a9a
unknown <bhushank0029@gmail.com>
2020-01-11 15:36:49 +0530

    First code

---
+++ src/seleniumprogram/NewTabOpen.java
@@ -0,0 +1,47 @@
+package seleniumprogram;
+
+import java.util.ArrayList;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class NewTabOpen 
+{
+	public static void main(String[] args) throws InterruptedException
+	{
+		System.setProperty("webdriver.chrome.driver", "D:\\Selenium jars\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://mail.rediff.com/cgi-bin/login.cgi");
+		driver.manage().window().maximize();
+		String selectnewtab = Keys.chord(Keys.CONTROL,Keys.RETURN);
+		driver.findElement(By.linkText("Create a new account")).sendKeys(selectnewtab);
+		
+		((JavascriptExecutor)driver).executeScript("window.open('about:blank','_blank')");
+		//driver.get("http://gmail.com");
+		
+		
+		// array list for geeting tab
+		ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
+		driver.switchTo().window(tabs.get(1));
+		driver.get("http://gmail.com");
+		driver.switchTo().window(tabs.get(2));
+
+
+		
+		Thread.sleep(5000);
+		driver.close();
+		/*Difference between in driver.close() and driver.quit()
+		 * driver.close()==> It will close the browser tabs
+		 * driver.open()==> It will quit the browser
+		 * */
+		
+		
+		
+		
+	}
+
+}
