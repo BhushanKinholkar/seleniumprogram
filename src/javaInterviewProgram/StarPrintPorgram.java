266a66ef1f3d7627000250df45a63ea0a8997e73
unknown <bhushank0029@gmail.com>
2020-03-19 20:29:41 +0530

    star program added

---
+++ src/javaInterviewProgram/StarPrintPorgram.java
@@ -0,0 +1,26 @@
+package javaInterviewProgram;
+
+public class StarPrintPorgram {
+
+	public static void main(String[] args) 
+	{
+		int n = 5;
+		for (int i = 1; i < n ; i++)
+		{
+			for(int k = 2*(n-i); k > 0 ; k--)
+			{
+				System.out.print(" ");
+			}
+		
+			for(int j = 0; j < i ; j++)
+			{
+				System.out.print("* ");
+			}
+			
+			System.out.println(" ");
+		}
+		
+		
+	}
+
+}
